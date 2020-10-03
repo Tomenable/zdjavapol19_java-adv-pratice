382dc85ebae7f4bbb29380206faf8418884ea58a
Przemyslaw Wozniak <inz.przemyslaw.wozniak@gmail.com>
2020-10-03 10:35:35 +0200

    OOP/ex1

--- src/main/java/pl/sda/zdjavapol19/Runner.java
+++ src/main/java/pl/sda/zdjavapol19/Runner.java
@@ -1,7 +1,16 @@
 package pl.sda.zdjavapol19;
 
+import pl.sda.zdjavapol19.oop.Point;
+
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Point pt1 = new Point();
+        Point pt2 = new Point(7, 7);
+
+        System.out.printf("Odleglosc pomiedzy %s a %s wynosi %f\n", pt1, pt2, pt1.distance(pt2));
+        System.out.printf("Odleglosc pomiedzy %s a %s wynosi %f\n", pt2, pt1, pt2.distance(pt1));
+        System.out.printf("Odleglosc pomiedzy %s a (0, 0) wynosi %f\n", pt2, pt2.distance(0,0));
+        System.out.printf("Odleglosc pomiedzy %s a poczatkiem ukladu wspolrzednych wynosi %f\n", pt2, pt2.distance());
+        System.out.printf("Odleglosc pomiedzy %s a poczatkiem ukladu wspolrzednych wynosi %f\n", pt1, pt1.distance());
     }
 }
--- src/main/java/pl/sda/zdjavapol19/oop/Point.java
+++ src/main/java/pl/sda/zdjavapol19/oop/Point.java
@@ -1,5 +1,58 @@
 package pl.sda.zdjavapol19.oop;
 
 public class Point {
+    private int x, y;   //=0
 
+    public Point() {}
+
+    //alt+insert -> generate -> constructor
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    //SRP - przekierowuje zadanie do jedynej metody odpowiadajacej za obliczenie
+    public double distance(int x, int y) {
+        return Math.sqrt(Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2));
+    }
+
+    public double distance() {  //0,0
+        return distance(0, 0);
+    }
+
+    public double distance(Point pt) {
+        return distance(pt.x, pt.y);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+
+    public int[] getXY() {
+        int tab[] = {x, y};
+        return tab;
+    }
+
+    //SRP = Single Responsibility Principle - zasada nr 1 z SOLID
+    public void setXY(int x, int y) {   //ta metoda jedynie przekierowuje zadnie do metody modyfikujacej
+        setX(x);
+        setY(y);
+    }
+
+    public int getX() {
+        return x;
+    }
+    //tylko ta metoda FIZYCZNIE odpowiada za modyfikacje pola
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
