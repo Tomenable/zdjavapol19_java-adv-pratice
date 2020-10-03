3d7ab2bd678267a857f345e9fce79211ffb15e2e
Przemyslaw Wozniak <inz.przemyslaw.wozniak@gmail.com>
2020-10-03 11:03:45 +0200

    OOP/ex2

--- src/main/java/pl/sda/zdjavapol19/Runner.java
+++ src/main/java/pl/sda/zdjavapol19/Runner.java
@@ -1,5 +1,6 @@
 package pl.sda.zdjavapol19;
 
+import pl.sda.zdjavapol19.oop.Line;
 import pl.sda.zdjavapol19.oop.Point;
 
 public class Runner {
@@ -12,5 +13,10 @@ public class Runner {
         System.out.printf("Odleglosc pomiedzy %s a (0, 0) wynosi %f\n", pt2, pt2.distance(0,0));
         System.out.printf("Odleglosc pomiedzy %s a poczatkiem ukladu wspolrzednych wynosi %f\n", pt2, pt2.distance());
         System.out.printf("Odleglosc pomiedzy %s a poczatkiem ukladu wspolrzednych wynosi %f\n", pt1, pt1.distance());
+
+        System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+        Line l1 = new Line(pt1, pt2);
+        System.out.println(l1);
+        System.out.println("Dlugosc linii l1 = " + l1.getLength());
     }
 }
---
+++ src/main/java/pl/sda/zdjavapol19/oop/Line.java
@@ -0,0 +1,66 @@
+package pl.sda.zdjavapol19.oop;
+
+public class Line {
+
+    private Point begin, end;
+
+    public Line(int x1, int y1, int x2, int y2) {
+        this.begin = new Point(x1, y1);
+        this.end = new Point(x2, y1);
+    }
+
+    public Line(Point begin, Point end) {
+        this.begin = begin;
+        this.end = end;
+    }
+
+    @Override   //przesloniecie; overloading - przeciazanie (vide metoda Point::distance)
+    public String toString() {
+        return String.format("Odcinek: [początek=%s, koniec=%s]", begin, end);
+    }
+
+    public double getLength() {
+        return begin.distance(end);
+    }
+
+    //analogicznie dla punktu end
+    public int getBeginX() {
+        return begin.getX();
+    }
+
+    public int getBeginY() {
+        return begin.getY();
+    }
+
+    public int[] getBeginXY() {
+        return begin.getXY();
+    }
+
+    public void setBeginX(int x) {
+        begin.setX(x);
+    }
+
+    public void setBeginY(int y) {
+        begin.setY(y);
+    }
+
+    public void setBeginXY(int x, int y) {
+        begin.setXY(x, y);
+    }
+
+    public Point getBegin() {
+        return begin;
+    }
+
+    public void setBegin(Point begin) {
+        this.begin = begin;
+    }
+
+    public Point getEnd() {
+        return end;
+    }
+
+    public void setEnd(Point end) {
+        this.end = end;
+    }
+}
